f6096e48745b783ecf4f981e672fa8245cc27b14
Boniface Mwandobo <bonifacesimon04@gmail.com>
2025-06-23 13:28:52 +0300

    introduction to licence

---
+++ src/app/licences/page.tsx
@@ -0,0 +1,171 @@
+"use client"
+
+import ProtectedRoute from '@/components/authentication/protected-route'
+import PageHeader from '@/components/header/page-header'
+import React from 'react'
+import {usePageDataHook} from "@/hooks/page-render-hooks/use-page-data.hook";
+import {checkPermissions} from "@/utils/check-permissions";
+function Contract() {
+    const permission = 'projects'
+
+    const projectInputs = [
+        {
+            name: 'title',
+            type: 'text',
+            label: "Licence Title",
+            value: '',
+            required: true,
+            isError: false,
+            errorMessage: ''
+        },
+        {
+            name: 'supplier_id',
+            type: 'select',
+            label: `Select Supplier`,
+            value: '',
+            optionsUrlData: `suppliers`,
+            optionDataKey: 'name',
+            required: true,
+            isError: false,
+            isRemoved: true,
+            errorMessage: '',
+            controlled_by: 'contract-group',
+            control: 'supplier'
+        },
+        {
+            name: 'department_id',
+            type: 'select',
+            label: `Select Department`,
+            value: '',
+            optionsUrlData: `departments`,
+            optionDataKey: 'name',
+            required: true,
+            isError: false,
+            errorMessage: '',
+            control_for: ''
+        },
+        {
+            name: 'startDate',
+            type: 'date',
+            label: 'Start Date',
+            value: '',
+            required: true,
+            isError: false,
+            errorMessage: ''
+        },
+        {
+            name: 'endDate',
+            type: 'date',
+            label: 'End Date',
+            value: '',
+            required: true,
+            isError: false,
+            errorMessage: ''
+        },
+        {
+            name: 'description',
+            type: 'textArea',
+            label: 'Description',
+            value: '',
+            required: true,
+            isError: false,
+            errorMessage: ''
+        },
+        {
+            name: 'file',
+            type: 'file',
+            label: "Upload File",
+            value: '',
+            placeholder: "upload file",
+            required: true,
+            isError: false,
+            errorMessage: ''
+        },
+    ]
+
+    const _columns = [
+        {
+            id: 'title',
+            numeric: false,
+            disablePadding: false,
+            label: 'Licence Title',
+        },
+        {
+            id: 'supplierName',
+            numeric: false,
+            disablePadding: false,
+            label: 'Supplier Name',
+        },
+        {
+            id: 'departmentName',
+            label: 'Department',
+            value: '',
+            required: true,
+            isError: false,
+            errorMessage: ''
+        },
+        {
+            id: 'startDate',
+            type: 'date',
+            label: 'Start Date',
+            value: '',
+            required: true,
+            isError: false,
+            errorMessage: ''
+        },
+        {
+            id: 'endDate',
+            numeric: false,
+            disablePadding: false,
+            label: 'End Date',
+        },
+        {
+            id: 'status',
+            numeric: false,
+            disablePadding: false,
+            label: 'Status',
+        },
+    ]
+
+    const {
+        loading,
+        createdForm,
+        handleClick,
+        tabular,
+    } = usePageDataHook({
+        columns: _columns,
+        formInputs: projectInputs,
+        url: 'licences',
+        modalTitle: 'Licence',
+        viewUrl: '/licences/',
+        state_properties: [],
+        permission: permission,
+        from: "licences",
+        isApiV2: true,
+        isFormData: true,
+        isMaintainViewNavigationForV1: true
+    })
+    return (
+        <ProtectedRoute>
+            <>{
+                !checkPermissions(`${permission}-list`) ? <p>You are not authorized</p> : <>
+                    {
+                        loading ? <p>Loading...</p>
+                            :
+                            <>
+                                <PageHeader
+                                    handleClick={handleClick}
+                                    links={[{ name: 'Licence / List', linkTo: '/licence', permission: '' }]}
+                                />
+                                {tabular()}
+                                {createdForm('md')}
+                            </>
+                    }
+                </>
+            }
+            </>
+        </ProtectedRoute>
+    )
+}
+
+export default Contract
