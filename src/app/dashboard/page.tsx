1ac108c570e3dbcee9c99a4a1ddaaa3e67d1d0ff
Boniface Mwandobo <bonifacesimon04@gmail.com>
2025-07-23 13:22:07 +0300

    dashboard stats

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,22 +7,13 @@ import React from 'react';
 import ProtectedRoute from "@/components/authentication/protected-route";
 import {checkPermissions} from "@/utils/check-permissions";
 import AccessDeniedComponent from "@/components/status/access-denied.component";
+import DashboardStatsFragment from "@/app/dashboard/fragments/dashboard-stats.fragment";
 
 const { Title, Text } = Typography;
 
 const DashboardPage = () => {
     // Dummy data - replace with real API calls
-    const stats = {
-        totalContracts: 124,
-        activeContracts: 89,
-        expiringSoon: 15,
-        totalLicenses: 76,
-        activeLicenses: 62,
-        expiredLicenses: 8,
-        suppliers: 42,
-        clients: 58,
-        departments: 12
-    };
+
 
     const contractTypes = [
         { type: 'Supplier Contracts', value: 68 },
@@ -70,107 +61,77 @@ const DashboardPage = () => {
         <div className="dashboard-container">
             <Title level={2}>Contract & License Management Dashboard</Title>
 
-            {/* Summary Stats Row */}
-            <Row gutter={16} style={{ marginBottom: 24 }}>
-                <Col span={4}>
-                    <Card>
-                        <Statistic title="Total Contracts" value={stats.totalContracts} />
-                    </Card>
-                </Col>
-                <Col span={4}>
-                    <Card>
-                        <Statistic title="Active Contracts" value={stats.activeContracts} />
-                    </Card>
-                </Col>
-                <Col span={4}>
-                    <Card>
-                        <Statistic title="Expiring Soon" value={stats.expiringSoon} />
-                    </Card>
-                </Col>
-                <Col span={4}>
-                    <Card>
-                        <Statistic title="Total Licenses" value={stats.totalLicenses} />
-                    </Card>
-                </Col>
-                <Col span={4}>
-                    <Card>
-                        <Statistic title="Active Licenses" value={stats.activeLicenses} />
-                    </Card>
-                </Col>
-                <Col span={4}>
-                    <Card>
-                        <Statistic title="Expired Licenses" value={stats.expiredLicenses} />
-                    </Card>
-                </Col>
-            </Row>
+            <DashboardStatsFragment />
+
+
 
             {/* Main Content Row */}
-            <Row gutter={16}>
-                {/* Left Column */}
-                <Col span={12}>
-                    <Card title="Contract Types" style={{ marginBottom: 16 }}>
-                        <Pie
-                            data={contractTypes}
-                            angleField="value"
-                            colorField="type"
-                            radius={0.8}
-                            label={{
-                                type: 'inner',
-                                content: '{name}',
-                            }}
-                        />
-                    </Card>
+            {/*<Row gutter={16}>*/}
+            {/*    /!* Left Column *!/*/}
+            {/*    <Col span={12}>*/}
+            {/*        <Card title="Contract Types" style={{ marginBottom: 16 }}>*/}
+            {/*            <Pie*/}
+            {/*                data={contractTypes}*/}
+            {/*                angleField="value"*/}
+            {/*                colorField="type"*/}
+            {/*                radius={0.8}*/}
+            {/*                label={{*/}
+            {/*                    type: 'inner',*/}
+            {/*                    content: '{name}',*/}
+            {/*                }}*/}
+            {/*            />*/}
+            {/*        </Card>*/}
 
-                    <Card title="License Status">
-                        <Pie
-                            data={licenseStatus}
-                            angleField="value"
-                            colorField="status"
-                            radius={0.8}
-                            label={{
-                                type: 'inner',
-                                content: '{name}',
-                            }}
-                        />
-                    </Card>
-                </Col>
+            {/*        <Card title="License Status">*/}
+            {/*            <Pie*/}
+            {/*                data={licenseStatus}*/}
+            {/*                angleField="value"*/}
+            {/*                colorField="status"*/}
+            {/*                radius={0.8}*/}
+            {/*                label={{*/}
+            {/*                    type: 'inner',*/}
+            {/*                    content: '{name}',*/}
+            {/*                }}*/}
+            {/*            />*/}
+            {/*        </Card>*/}
+            {/*    </Col>*/}
 
-                {/* Right Column */}
-                <Col span={12}>
-                    <Card title="Upcoming Expirations" style={{ marginBottom: 16 }}>
-                        <Line
-                            data={expirationData}
-                            xField="month"
-                            yField="contracts"
-                            seriesField="type"
-                            color={['#1890ff', '#13c2c2']}
-                            yAxis={{
-                                label: {
-                                    formatter: (v) => `${v} documents`,
-                                },
-                            }}
-                            legend={{
-                                position: 'top',
-                            }}
-                            smooth
-                        />
-                    </Card>
+            {/*    /!* Right Column *!/*/}
+            {/*    <Col span={12}>*/}
+            {/*        <Card title="Upcoming Expirations" style={{ marginBottom: 16 }}>*/}
+            {/*            <Line*/}
+            {/*                data={expirationData}*/}
+            {/*                xField="month"*/}
+            {/*                yField="contracts"*/}
+            {/*                seriesField="type"*/}
+            {/*                color={['#1890ff', '#13c2c2']}*/}
+            {/*                yAxis={{*/}
+            {/*                    label: {*/}
+            {/*                        formatter: (v) => `${v} documents`,*/}
+            {/*                    },*/}
+            {/*                }}*/}
+            {/*                legend={{*/}
+            {/*                    position: 'top',*/}
+            {/*                }}*/}
+            {/*                smooth*/}
+            {/*            />*/}
+            {/*        </Card>*/}
 
-                    <Card title="By Department">
-                        <Bar
-                            data={departmentDistribution}
-                            xField="department"
-                            yField={['contracts', 'licenses']}
-                            isStack
-                            seriesField="type"
-                            marginRatio={0}
-                            label={{
-                                position: 'middle',
-                            }}
-                        />
-                    </Card>
-                </Col>
-            </Row>
+            {/*        <Card title="By Department">*/}
+            {/*            <Bar*/}
+            {/*                data={departmentDistribution}*/}
+            {/*                xField="department"*/}
+            {/*                yField={['contracts', 'licenses']}*/}
+            {/*                isStack*/}
+            {/*                seriesField="type"*/}
+            {/*                marginRatio={0}*/}
+            {/*                label={{*/}
+            {/*                    position: 'middle',*/}
+            {/*                }}*/}
+            {/*            />*/}
+            {/*        </Card>*/}
+            {/*    </Col>*/}
+            {/*</Row>*/}
 
             {/* Recent Activities */}
             <Card title="Recent Activities" style={{ marginTop: 16 }}>
