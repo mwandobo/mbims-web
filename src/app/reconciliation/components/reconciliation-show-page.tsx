17b31167e0f5af694871f00da6fae18682189984
boniface mwandobo <bonifacesimon04@gmail.com>
2026-09-16 13:51:27 +0300

    Introduction to reconciliation show page

--- src/app/reconciliation/components/reconciliation-show-page.tsx
+++ src/app/reconciliation/components/reconciliation-show-page.tsx
@@ -12,17 +12,17 @@ import {ButtonComponent} from "@/components/button/button.component";
 import {showConfirmationModal} from "@/utils/show-alert-dialog";
 import {checkPermissions} from "@/utils/check-permissions";
 
-export default function ReconciliationShowPage({employeeId}: { employeeId: string }) {
-    const permission = 'employee'
+export default function ReconciliationShowPage({reconciliationId}: { reconciliationId: string }) {
+    const permission = 'reconciliation'
     const router = useRouter()
     const [data, setData] = useState<any>([])
     const [loading, setLoading] = useState(false)
     const [refresh, setRefresh] = useState(false)
 
 
-    const id = employeeId
+    const id = reconciliationId
 
-    const url = `administration/employees/${id}`
+    const url = `reconciliations/${id}`
     const navigateToLogin = () => {
         return router.push('/login')
     }
@@ -51,25 +51,6 @@ export default function ReconciliationShowPage({employeeId}: { employeeId: strin
     }, [refresh])
 
 
-    const onSave = async () => {
-        try {
-            const res = await postRequest(`${url}/share-credential`, {});
-            if (data && res.status === 200) {
-                setRefresh(!refresh);
-            }
-        } catch (error: any) {
-            console.log(error);
-        }
-    };
-
-    const handleSubmit = () => {
-        showConfirmationModal({
-            title: 'Are You Sure?',
-            text: `Are You Sure You Want Share Credentials with: ${data?.name}?`,
-            onConfirm: onSave,  // Action to perform on confirmation
-            onCancel: () => console.log('User canceled the action'), // Optional cancel action
-        });
-    };
 
     return (
 
@@ -79,46 +60,23 @@ export default function ReconciliationShowPage({employeeId}: { employeeId: strin
         >
             <PageHeader
                 links={[
-                    {name: 'Employee', linkTo: '/admnistration/employees', permission: 'employee', isClickable: true},
-                    {name: 'Show', linkTo: '/admnistration/employees/show', permission: ''},]}
+                    {name: 'Reconciliation', linkTo: '/reconciliation', permission: 'reconciliation', isClickable: true},
+                    {name: 'Show', linkTo: '/reconciliation/show', permission: ''},]}
                 isShowPage={true}
             />
             <MuiCardComponent>
 
-                {data.email && checkPermissions(`${permission}_share_credential`) && (
-                    <div>
-                        <ButtonComponent
-                            name={`${data.isCredentialShared ? "Resend Credentials" : "Share Credentials"}`}
-                            onClick={handleSubmit}
-                            rounded={'md'}
-                            padding={'p-3'}
-                            shadow={'shadow-md'}
-                            bg_color={'bg-gray-50'}
-                            hover={'hover:bg-gray-200 hover:border-gray-400'}
-                            hover_text={'hover:text-gray-900 hover:font-semibold'}
-                            border={'border border-gray-300'}
-                            text_color={'text-gray-700'}
-                        >
-                            <CheckCircle2 size={13}/>
-                        </ButtonComponent>
-                    </div>
-                )}
 
                 <ViewCardComponent
                     data={[
-                        {label: 'Employee Name', value: data.name},
-                        {label: 'Staff No', value: data?.staffNo},
-                        {label: 'Email', value: data?.email},
-                        {label: 'Phone', value: data?.mobilePhone},
-                        {label: 'Date Joined', value: data?.createdAt},
-                        {label: 'Gender', value: data?.gender},
-                        {label: 'Unit', value: data?.unitName ?? '---'},
-                        {label: 'Gender', value: data?.gender},
-                        {label: 'Department', value: data?.departmentName ?? '---'},
-                        {label: 'Position', value: data?.positionName ?? '---'},
+                        {label: 'Reconciliation Code', value: data.code},
+                        {label: 'Reconciliation Date', value: data?.createdAt},
+                        {label: 'Staff Name', value: data?.staffName},
+                        {label: 'Reconciliation Status', value: data?.status}
+
                     ]}
-                    titleA="Employee"
-                    titleB={data.name}
+                    titleA="Reconciliation"
+                    titleB={data.code}
                 />
             </MuiCardComponent>
         </ProtectedRoute>
